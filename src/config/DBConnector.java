f469377b31688e88395a9983fdb494a2ba3fcf4a
Đức Em <ducemtran44@gmail.com>
2024-03-31 23:24:57 +0700

    DBConnector

---
+++ src/config/DBConnector.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package config;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Admin
+ */
+public class DBConnector {
+    private static String url = "jdbc:mysql://localhost:3306/test";
+    private static String userName = "root";
+    private static String passWord = "";
+    
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            conn = DriverManager.getConnection(url, userName, passWord);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Không thể kết nối đến cơ sở dữ liệu !", "Lỗi", JOptionPane.ERROR_MESSAGE);
+        }
+        return conn;
+    }
+    
+    public static void closeConnection(Connection c) {
+        try {
+            if (c != null) {
+                c.close();
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
+}
